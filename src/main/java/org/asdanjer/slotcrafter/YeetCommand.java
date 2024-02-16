cce435ab653afbda39d6814af66163f6ee392dec
asdanjer <17290236+asdanjer@users.noreply.github.com>
2024-02-16 03:26:03 +0100

    added custome yeet mspt

--- src/main/java/org/asdanjer/slotcrafter/YeetCommand.java
+++ src/main/java/org/asdanjer/slotcrafter/YeetCommand.java
@@ -6,23 +6,23 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
+import java.util.HashMap;
 import java.util.UUID;
 
 public class YeetCommand implements CommandExecutor {
     private Slotcrafter plugin;
     private HashSet<UUID> yeetablePlayers;
+    private HashMap<UUID, Double> customMsptThresholds;
 
     public YeetCommand(Slotcrafter plugin) {
         this.plugin = plugin;
         this.yeetablePlayers = new HashSet<>();
+        this.customMsptThresholds = new HashMap<>();
     }
 
-@Override
-public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-    if (command.getName().equalsIgnoreCase("yeetme")) {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (command.getName().equalsIgnoreCase("yeetme")) {
             if (!(sender instanceof Player)) {
                 sender.sendMessage("This command can only be used by a player.");
@@ -35,51 +35,71 @@ public boolean onCommand(CommandSender sender, Command command, String label, St
             Player player = (Player) sender;
             if (yeetablePlayers.contains(player.getUniqueId())) {
                 yeetablePlayers.remove(player.getUniqueId());
+                customMsptThresholds.remove(player.getUniqueId());
                 player.sendMessage("You will not be automatically kicked anymore.");
             } else {
                 yeetablePlayers.add(player.getUniqueId());
-                player.sendMessage("You will be automatically kicked when MSPT gets too high.");
+                if (args.length > 0) {
+                    try {
+                        double customMspt = (double) Integer.parseInt(args[0]);
+                        customMsptThresholds.put(player.getUniqueId(), customMspt);
+                        player.sendMessage("You will be automatically kicked when MSPT gets to " + customMspt + ".");
+                    } catch (NumberFormatException e) {
+                        player.sendMessage("Invalid MSPT value. Using default.");
+                    }
+                } else {
+                    player.sendMessage("You will be automatically kicked when MSPT gets too high.");
+                }
             }
-        }
-        return true;
-    } else if (command.getName().equalsIgnoreCase("yeetthem")) {
-        if (!sender.hasPermission("slotcrafter.yeetthem")) {
-            sender.sendMessage("You do not have permission to use this command.");
+            return true;
+        } else if (command.getName().equalsIgnoreCase("yeetthem")) {
+            if (!sender.hasPermission("slotcrafter.yeetthem")) {
+                sender.sendMessage("You do not have permission to use this command.");
+                return true;
+            }
+            int yeetlist=yeetPlayers();
+            sender.sendMessage(yeetlist+ " Players have been yeeted!");
             return true;
         }
-        int yeetlist=yeetPlayers();
-        sender.sendMessage(yeetlist+ " Players have been yeeted!");
-        return true;
+        return false;
     }
-    return false;
-}
+
     public void removeyeeter(UUID player) {
         if (yeetablePlayers.contains(player)) {
             yeetablePlayers.remove(player);
+            customMsptThresholds.remove(player);
         }
     }
+
     public void checkyeetability() {
         double mspt = plugin.getMspt();
-        double kickmspt = plugin.getConfig().getDouble("kickmspt");
 
-        if (mspt > kickmspt) {
-            yeetPlayers();
+        for (UUID playerId : yeetablePlayers) {
+            double kickmspt = customMsptThresholds.getOrDefault(playerId, plugin.getConfig().getDouble("kickmspt"));
+            if (mspt > kickmspt) {
+                yeetPlayer(playerId);
+            }
         }
+    }
+
+    private void yeetPlayer(UUID playerId) {
+        Player yeetablePlayer = Bukkit.getPlayer(playerId);
+        if (yeetablePlayer != null) {
+            yeetablePlayer.kickPlayer("MSPT is too high!");
+            customMsptThresholds.remove(playerId);
+        }
+        yeetablePlayers.remove(playerId);
     }
 
     private int yeetPlayers() {
-        List<UUID> playersToYeet = new ArrayList<>(yeetablePlayers);
-        int yeetlist=playersToYeet.size();
-        for (UUID playerId : playersToYeet) {
-            Player yeetablePlayer = Bukkit.getPlayer(playerId);
-            if (yeetablePlayer != null) {
-                yeetablePlayer.kickPlayer("MSPT is too high!");
-
-            }
+        int yeetedPlayers = 0;
+        for (UUID playerId : yeetablePlayers) {
+            yeetPlayer(playerId);
+            yeetedPlayers++;
         }
-        yeetablePlayers.clear();
-        return yeetlist;
+        return yeetedPlayers;
     }
+
     public HashSet<UUID> getYeetablePlayers() {
         return yeetablePlayers;
     }
