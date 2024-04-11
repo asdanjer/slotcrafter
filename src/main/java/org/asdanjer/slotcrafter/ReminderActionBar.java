f49967719dda5d7594b67e1158c6022c2961c0f8
asdanjer <17290236+asdanjer@users.noreply.github.com>
2024-04-11 14:06:56 +0200

    fixed a bug where having 1 of the action bar intervalls to 0 and the outher  to >0 would caus unexpected behavior.

--- src/main/java/org/asdanjer/slotcrafter/ReminderActionBar.java
+++ src/main/java/org/asdanjer/slotcrafter/ReminderActionBar.java
@@ -22,43 +22,48 @@ public class ReminderActionBar {
     }
 
     public void start() {
-        new BukkitRunnable() {
-            @Override
-            public void run() {
-                for (Player player : Bukkit.getOnlinePlayers()) {
-                    UUID playerId = player.getUniqueId();
-                    try {
-                        if (yeetCommand.getYeetablePlayers().contains(playerId)) {
-                            player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(plugin.getConfig().getString("YeetmeMessage")));
+        if(plugin.getConfig().getLong("ReminderInterval")>0) {
+            new BukkitRunnable() {
+                @Override
+                public void run() {
+                    for (Player player : Bukkit.getOnlinePlayers()) {
+                        UUID playerId = player.getUniqueId();
+                        try {
+                            if (yeetCommand.getYeetablePlayers().contains(playerId)) {
+                                player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(plugin.getConfig().getString("YeetmeMessage")));
+                            }
+                            if (takeMySlotCommand.getSlotOfferedPlayers().containsKey(playerId)) {
+                                new BukkitRunnable() {
+                                    @Override
+                                    public void run() {
+                                        player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(plugin.getConfig().getString("TakemyslotMessage")));
+                                    }
+                                }.runTaskLater(plugin, 40);
+                            }
+                        } catch (Exception e) {
+                            Bukkit.getLogger().warning("Action bar config missung or wrong. Please check your config file.");
+                            this.cancel();
                         }
-                        if (takeMySlotCommand.getSlotOfferedPlayers().containsKey(playerId)) {
-                            new BukkitRunnable() {
-                                @Override
-                                public void run() {
-                                    player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(plugin.getConfig().getString("TakemyslotMessage")));
-                                }
-                            }.runTaskLater(plugin, 40);                        }
-                    }catch (Exception e) {
-                        Bukkit.getLogger().warning("Action bar config missung or wrong. Please check your config file.");
-                        this.cancel();
                     }
                 }
-            }
-        }.runTaskTimer(plugin, 0, plugin.getConfig().getLong("ReminderInterval") * 20);
-        new BukkitRunnable() {
-            @Override
-            public void run() {
-                for (Player player : Bukkit.getOnlinePlayers()) {
-                    try {
-                        if (info.getCurrentlyUsedMSPT() > plugin.getConfig().getInt("HighMSPTThreshold")) {
-                            player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(plugin.getConfig().getString("HighMSPTWarningMessage")));
+            }.runTaskTimer(plugin, 0, plugin.getConfig().getLong("ReminderInterval") * 20);
+        }
+        if(plugin.getConfig().getLong("HighMSPTWarningInterval")>0) {
+            new BukkitRunnable() {
+                @Override
+                public void run() {
+                    for (Player player : Bukkit.getOnlinePlayers()) {
+                        try {
+                            if (info.getCurrentlyUsedMSPT() > plugin.getConfig().getInt("HighMSPTThreshold")) {
+                                player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(plugin.getConfig().getString("HighMSPTWarningMessage")));
+                            }
+                        } catch (Exception e) {
+                            Bukkit.getLogger().warning("Action bar config missing or wrong. Please check your config file.");
+                            this.cancel();
                         }
-                    }catch (Exception e) {
-                        Bukkit.getLogger().warning("Action bar config missing or wrong. Please check your config file.");
-                        this.cancel();
                     }
                 }
-            }
-        }.runTaskTimer(plugin, 80, plugin.getConfig().getLong("HighMSPTWarningInterval") * 20);
+            }.runTaskTimer(plugin, 80, plugin.getConfig().getLong("HighMSPTWarningInterval") * 20);
+        }
     }
 }
